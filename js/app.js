b9d99119171e50603d6575a7a5b99f4d0a591bd1
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-05 04:09:16 +0100

    + Refactor out enemy module.

--- js/app.js
+++ js/app.js
@@ -6,47 +6,15 @@ var COLUMN_COUNT = 8;
 var PLAYER_WIDTH = 101;
 var PLAYER_HEIGHT = 101;
 
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(ImageLoader.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [];
-var enemyCount = 4;
+var enemyCount = 1;
 var player = null;
 
 /*
     Create enemy entities.
 */
 for (var count = enemyCount; count > 0; count--) {
-    allEnemies.push(new Enemy());
+    allEnemies.push(new Enemy(0, 0));
 }
 
 /*
